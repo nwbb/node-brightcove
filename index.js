a2c44de49c7e813f9e9d63c69c6766a010e3ed77
wmcmurray <tiwilinlove@hotmail.com>
2015-10-26 14:21:33 -0400

    Implement createAccessToken of OAuth API

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var MediaApi = require('./lib/mediaApi'),
+	OAuthApi = require('./lib/oauthApi'),
 	Playlist = require('./lib/playlist'),
 	Errors = require('./lib/errors');
 
 module.exports = {
 	Api: MediaApi, // old and deprecated!!
 	MediaApi: MediaApi,
+	OAuthApi: OAuthApi,
 	Playlist: Playlist,
 	errors: Errors
 }
